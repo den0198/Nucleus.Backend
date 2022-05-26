7176d6c1c7df90dc3dfb88a22551c413c1c9ecef
Салихов Даниил <dsalikhov@enaza.games>
2022-05-26 19:41:58 +0600

    add docker

--- Api/Extensions/Middlewares/InitializationDataBaseMiddlewareExtension.cs
+++ Api/Extensions/Middlewares/InitializationDataBaseMiddlewareExtension.cs
@@ -1,4 +1,6 @@
 ﻿using API.Initialization;
+using DAL.EntityFramework;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions.Middlewares;
 
@@ -6,6 +8,9 @@ public static class InitializationDataBaseMiddlewareExtension
 {
     public static async void UseInitializationDataBase(this IApplicationBuilder applicationBuilder)
     {
+        await using var scope = applicationBuilder.ApplicationServices.CreateAsyncScope();
+        await scope.ServiceProvider.GetService<AppDbContext>()?.Database.MigrateAsync()!;
+
         await Seeds.InitialSeeds(applicationBuilder);
     }
 }
