b714c0a2fba00e2617f0b75fcfdd005d19c9f1e0
den0198 <salikhovden0198@gmail.com>
2023-04-30 17:20:16 +0600

    fix

--- Api/Extensions/Middlewares/InitializationDataBaseMiddlewareExtension.cs
+++ Api/Extensions/Middlewares/InitializationDataBaseMiddlewareExtension.cs
@@ -4,6 +4,7 @@ namespace Nucleus.API.Extensions.Middlewares;
 
 public static class InitializationDataBaseMiddlewareExtension
 {
+    //TODO: Async void зло но сдесь вроде как уместно, ну вдруг можно лучше)))
     public static async void UseInitializationDataBase(this IApplicationBuilder applicationBuilder)
     {
         await Seeds.InitialSeeds(applicationBuilder);
