4391b332d55c482c82bfa56e317db5515faf3b76
den <salhovden0198@gmail.com>
2023-05-10 21:03:33 +0600

    fix error

--- Nucleus.API/Filters/GraphQlExceptionFilter.cs
+++ Nucleus.API/Filters/GraphQlExceptionFilter.cs
@@ -4,10 +4,21 @@ namespace Nucleus.API.Filters;
 
 public class GraphQlExceptionFilter : IErrorFilter
 {
+    private readonly ILogger<GraphQlExceptionFilter> logger;
+    public GraphQlExceptionFilter(ILogger<GraphQlExceptionFilter> logger)
+    {
+        this.logger = logger;
+    }
+    
     public IError OnError(IError error)
     {
-        return error.WithCode(error.Exception is CoreException t 
-            ? t.Code.ToString() 
-            : "500");
+        var code = error.Exception is CoreException t
+            ? t.Code.ToString()
+            : "500";
+        logger.LogError($"Error with code: '{code}' " +
+                        $"and exception message: {error.Exception?.Message}," +
+                        $"and error message: {error.Message}");
+        
+        return error.WithCode(code);
     }
 }
