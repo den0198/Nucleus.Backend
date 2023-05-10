be4e79c96fdf4208deb22916a874d2be05f4766a
den <salhovden0198@gmail.com>
2023-05-10 20:40:31 +0600

    add nlog

--- Nucleus.API/Program.cs
+++ Nucleus.API/Program.cs
@@ -1,3 +1,5 @@
+using NLog;
+using NLog.Web;
 using Nucleus.API.Extensions.Middlewares;
 using Nucleus.API.Extensions.Services;
 using Nucleus.Common.MapperConfigurations;
@@ -8,31 +10,50 @@ public class Program
 {
     public static void Main(string [] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-        var services = builder.Services;
-        var configuration = builder.Configuration;
-
-        CoreMapperConfiguration.AddConfigurations();
-
-        services.AddAppCors();
-        services.AddControllers();
-        services.AddAppAuth(configuration);
-        services.AddAppGraphQl();
-        services.AddAppEntityFramework(configuration);
-        services.AddAppUnitOfWork();
-        services.AddAppOptions(configuration);
-        services.AddAppInitialParams();
-        services.AddAppServices();
-        services.AddAppFilters();
-
-        var app = builder.Build();
+        var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+        logger.Debug("init main");
         
-        app.UseCors("MyAllowAllHeadersPolicy");
-        app.UseInitializationDataBase();
-        app.MapGraphQL("/");
-        app.UseRouting();
-        app.UseAuth();
+        try
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            
+            builder.Logging.ClearProviders();
+            builder.Host.UseNLog();
+            
+            var services = builder.Services;
+            var configuration = builder.Configuration;
 
-        app.Run();
+            CoreMapperConfiguration.AddConfigurations();
+
+            services.AddAppCors();
+            services.AddControllers();
+            services.AddAppAuth(configuration);
+            services.AddAppGraphQl();
+            services.AddAppEntityFramework(configuration);
+            services.AddAppUnitOfWork();
+            services.AddAppOptions(configuration);
+            services.AddAppInitialParams();
+            services.AddAppServices();
+            services.AddAppFilters();
+
+            var app = builder.Build();
+        
+            app.UseCors("MyAllowAllHeadersPolicy");
+            app.UseInitializationDataBase();
+            app.MapGraphQL("/");
+            app.UseRouting();
+            app.UseAuth();
+
+            app.Run();
+        }
+        catch (Exception exception)
+        {
+            logger.Error(exception, "Stopped program because of exception");
+            throw;
+        }
+        finally
+        {
+            LogManager.Shutdown();
+        }
     }
 }
